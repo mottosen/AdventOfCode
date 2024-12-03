6b3eefb1be71630bd4b06208a604992174765870
Marius Ottosen <marius@appstract.co>
2024-12-03 16:12:52 +0100

    Day 3 a little nicer.

--- AOC24/Day03.fs
+++ AOC24/Day03.fs
@@ -4,30 +4,24 @@ open System
 open System.Text.RegularExpressions
 
 type Day3() =
-    // multiply numbers, contained in regex match, add to acc
-    static member private mulAcc (acc : int) (m : Match) : int =
-        acc + ((m.Groups.[1].Value |> Int32.Parse) * (m.Groups.[2].Value |> Int32.Parse))
+    static member private reg : Regex = new Regex("(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))")
 
+    // multiply numbers, contained in regex match, add to acc
+    static member private mulAcc (acc : int) : Match -> int = function
+        | m when not m.Groups.[1].Success -> acc
+        | m -> acc + ((m.Groups.[2].Value |> Int32.Parse) * (m.Groups.[3].Value |> Int32.Parse))
+    
     // extract multiplications with regex, sum across lines in input
     static member Star1 : string[] -> string =
-        Array.Parallel.map (fun l ->
-            Regex.Matches(l, "mul\((\d{1,3}),(\d{1,3})\)") |> Seq.fold Day3.mulAcc 0)
-        >> Array.fold (+) 0 >> string
+        string << Array.fold (+) 0 << Array.Parallel.map (fun l ->
+            (Day3.reg.Matches l) |> Seq.fold Day3.mulAcc 0)
 
     // extend Star1 with 'enable switch', state carries over between lines in input
     static member Star2 : string[] -> string =
-        let lineRes (state : int*bool) (line : string) : int*bool =
-            let rec helper (res : int, enabled : bool) : Match list -> int*bool = function
-                | m::ms ->
-                    // regex allows assumptions here
-                    if (m.Groups.[0].Value = "do()") then helper (res, true) ms
-                    elif (m.Groups.[0].Value = "don't()") then helper (res, false) ms
-                    else
-                        if enabled then helper ((Day3.mulAcc res m), enabled) ms
-                        else helper (res, enabled) ms
-                | _ -> (res, enabled)
+        let matchHandler (res : int, enabled : bool) : Match -> int*bool = function
+            | m when m.Groups.[4].Success -> (res,true)                      // do
+            | m when m.Groups.[5].Success -> (res, false)                    // don't
+            | m -> ((if enabled then (Day3.mulAcc res m) else res), enabled) // mul
 
-            Regex.Matches(line, "mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)")
-            |> Seq.toList |> helper state
-
-        Array.fold lineRes (0, true) >> fst >> string
+        string << fst << Array.fold (fun s ->
+            Seq.fold matchHandler s << Day3.reg.Matches) (0, true)
