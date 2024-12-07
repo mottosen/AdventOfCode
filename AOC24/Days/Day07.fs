542eb862c1cecde411b9a8b011f61bca4e8b22b6
Marius Ottosen <marius@appstract.co>
2024-12-07 20:42:35 +0100

    Day 7 done.

---
+++ AOC24/Days/Day07.fs
@@ -0,0 +1,25 @@
+namespace AdventOfCode24
+
+open System.Text.RegularExpressions
+
+type Day7() =
+    // check validity of line, accumulating valid test results
+    static member private handleLine (helper : float -> float -> string list -> float) (count : float) (line : string) : float =
+        let nums = Regex.Matches(line, "\d+") |> Seq.map (fun m -> m.Value) |> Seq.toList
+        count + (helper (nums.Head |> float) 0. (nums.Tail))
+
+    static member Star1 : string[] -> string =
+        // tries different possible combinations, picking one (of potentially more) correct
+        let rec helper (res : float) (acc : float) : string list -> float = function
+            | n::ns -> (helper res (acc + (n |> float)) ns) |> max <| (helper res ((max acc 1.) * (n |> float)) ns)
+            | [] -> if res = acc then res else 0.
+
+        string << Array.fold (fun acc line -> Day7.handleLine helper acc line) 0.
+
+    static member Star2 : string[] -> string =
+        // extended Star 1 logic with concat operator
+        let rec helper (res : float) (acc : float) : string list -> float = function
+            | n::ns -> (helper res (acc + (n |> float)) ns) |> max <| (helper res ((max acc 1.) * (n |> float)) ns) |> max <| (helper res (((string acc) + n) |> float) ns)
+            | [] -> if res = acc then res else 0.
+
+        string << Array.fold (fun acc line -> Day7.handleLine helper acc line) 0.
