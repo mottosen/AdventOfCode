b92583888d3585fc6b49646e5cb216787a26896f
Marius Ottosen <marius@appstract.co>
2024-12-10 11:55:33 +0100

    Better timing.

--- AOC24/Program.fsx
+++ AOC24/Program.fsx
@@ -7,119 +7,139 @@ module Program =
     let main (args : string[]) : int =
         try
             let mutable day = 0
-            let timer = System.Diagnostics.Stopwatch.StartNew()
+            let timer = System.Diagnostics.Stopwatch()
 
             if (args.Length <> 1 || not (Int32.TryParse(args[0], &day))) then
                 raise (new ArgumentException("Program must take exactly one argument of the day to run."))
 
             if day = 1 then
-                Day1.Star1 (InputLoader.GetInputFromFile "inputs_test/day01_1.txt")
-                //Day1.Star1 (InputLoader.GetInputFromFile "inputs_real/day01.txt")
-                |> printfn "Day 1, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day1.Star2 (InputLoader.GetInputFromFile "inputs_test/day01_2.txt")
-                //Day1.Star2 (InputLoader.GetInputFromFile "inputs_real/day01.txt")
-                |> printfn "Day 1, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day01_1.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day01_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day01.txt"
+
+                timer.Start()
+                input |> Day1.Star1 |> printfn "Day 1, Star 1: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day1.Star2 |> printfn "Day 1, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 2 then
-                Day2.Star1 (InputLoader.GetInputFromFile "inputs_test/day02_1.txt")
-                //Day2.Star1 (InputLoader.GetInputFromFile "inputs_real/day02.txt")
-                |> printfn "Day 2, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day2.Star2 (InputLoader.GetInputFromFile "inputs_test/day02_2.txt")
-                //Day2.Star2 (InputLoader.GetInputFromFile "inputs_real/day02.txt")
-                |> printfn "Day 2, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day02_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day02_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day02.txt"
+
+                timer.Start()
+                input |> Day2.Star1 |> printfn "Day 2, Star 2: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day2.Star2 |> printfn "Day 2, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 3 then
-                Day3.Star1 (InputLoader.GetInputFromFile "inputs_test/day03_1.txt")
-                //Day3.Star1 (InputLoader.GetInputFromFile "inputs_real/day03.txt")
-                |> printfn "Day 3, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day3.Star2 (InputLoader.GetInputFromFile "inputs_test/day03_2.txt")
-                //Day3.Star2 (InputLoader.GetInputFromFile "inputs_real/day03.txt")
-                |> printfn "Day 3, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day03_3.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day03_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day03.txt"
+
+                timer.Start()
+                input |> Day3.Star1 |> printfn "Day 3, Star 3: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day3.Star2 |> printfn "Day 3, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 4 then
-                Day4.Star1 (InputLoader.GetInputFromFile "inputs_test/day04_1.txt")
-                //Day4.Star1 (InputLoader.GetInputFromFile "inputs_real/day04.txt")
-                |> printfn "Day 4, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day4.Star2 (InputLoader.GetInputFromFile "inputs_test/day04_2.txt")
-                //Day4.Star2 (InputLoader.GetInputFromFile "inputs_real/day04.txt")
-                |> printfn "Day 4, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day04_4.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day04_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day04.txt"
+
+                timer.Start()
+                input |> Day4.Star1 |> printfn "Day 4, Star 4: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day4.Star2 |> printfn "Day 4, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 5 then
-                Day5.Star1 (InputLoader.GetInputFromFile "inputs_test/day05_1.txt")
-                //Day5.Star1 (InputLoader.GetInputFromFile "inputs_real/day05.txt")
-                |> printfn "Day 5, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day5.Star2 (InputLoader.GetInputFromFile "inputs_test/day05_2.txt")
-                //Day5.Star2 (InputLoader.GetInputFromFile "inputs_real/day05.txt")
-                |> printfn "Day 5, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day05_5.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day05_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day05.txt"
+
+                timer.Start()
+                input |> Day5.Star1 |> printfn "Day 5, Star 5: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day5.Star2 |> printfn "Day 5, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 6 then
-                Day6.Star1 (InputLoader.GetInputFromFile "inputs_test/day06_1.txt")
-                //Day6.Star1 (InputLoader.GetInputFromFile "inputs_real/day06.txt")
-                |> printfn "Day 6, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day6.Star2 (InputLoader.GetInputFromFile "inputs_test/day06_2.txt")
-                //Day6.Star2 (InputLoader.GetInputFromFile "inputs_real/day06.txt")
-                |> printfn "Day 6, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day06_6.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day06_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day06.txt"
+
+                timer.Start()
+                input |> Day6.Star1 |> printfn "Day 6, Star 6: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day6.Star2 |> printfn "Day 6, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 7 then
-                Day7.Star1 (InputLoader.GetInputFromFile "inputs_test/day07_1.txt")
-                //Day7.Star1 (InputLoader.GetInputFromFile "inputs_real/day07.txt")
-                |> printfn "Day 7, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day7.Star2 (InputLoader.GetInputFromFile "inputs_test/day07_2.txt")
-                //Day7.Star2 (InputLoader.GetInputFromFile "inputs_real/day07.txt")
-                |> printfn "Day 7, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day07_7.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day07_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day07.txt"
+
+                timer.Start()
+                input |> Day7.Star1 |> printfn "Day 7, Star 7: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day7.Star2 |> printfn "Day 7, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 8 then
-                Day8.Star1 (InputLoader.GetInputFromFile "inputs_test/day08_1.txt")
-                //Day8.Star1 (InputLoader.GetInputFromFile "inputs_real/day08.txt")
-                |> printfn "Day 8, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day8.Star2 (InputLoader.GetInputFromFile "inputs_test/day08_2.txt")
-                //Day8.Star2 (InputLoader.GetInputFromFile "inputs_real/day08.txt")
-                |> printfn "Day 8, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day08_8.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day08_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day08.txt"
+
+                timer.Start()
+                input |> Day8.Star1 |> printfn "Day 8, Star 8: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day8.Star2 |> printfn "Day 8, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 9 then
-                Day9.Star1 (InputLoader.GetInputFromFile "inputs_test/day09_1.txt")
-                //Day9.Star1 (InputLoader.GetInputFromFile "inputs_real/day09.txt")
-                |> printfn "Day 9, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day9.Star2 (InputLoader.GetInputFromFile "inputs_test/day09_2.txt")
-                //Day9.Star2 (InputLoader.GetInputFromFile "inputs_real/day09.txt")
-                |> printfn "Day 9, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day09_9.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day09_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day09.txt"
+
+                timer.Start()
+                input |> Day9.Star1 |> printfn "Day 9, Star 9: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day9.Star2 |> printfn "Day 9, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             elif day = 10 then
-                Day10.Star1 (InputLoader.GetInputFromFile "inputs_test/day10_1.txt")
-                //Day10.Star1 (InputLoader.GetInputFromFile "inputs_real/day10.txt")
-                |> printfn "Day 10, Star 1: %s"
-                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds); timer.Restart()
-        
-                Day10.Star2 (InputLoader.GetInputFromFile "inputs_test/day10_2.txt")
-                //Day10.Star2 (InputLoader.GetInputFromFile "inputs_real/day10.txt")
-                |> printfn "Day 10, Star 2: %s"
+                let input = InputLoader.GetInputFromFile "inputs_test/day10_1.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_test/day10_2.txt"
+                //let input = InputLoader.GetInputFromFile "inputs_real/day10.txt"
+
+                timer.Start()
+                input |> Day10.Star1 |> printfn "Day 10, Star 10: %s"
+                printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
+                
+                timer.Restart()
+                input |> Day10.Star2 |> printfn "Day 10, Star 2: %s"
                 printfn "\ttime: %i ms" (timer.ElapsedMilliseconds)
 
             else
