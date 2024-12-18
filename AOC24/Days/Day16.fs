650a5e7392c5ee5fa53c82556ced3f397034a912
Marius Ottosen <marius@appstract.co>
2024-12-18 17:19:47 +0100

    Day 16 correct results, finally.

--- AOC24/Days/Day16.fs
+++ AOC24/Days/Day16.fs
@@ -1,12 +1,10 @@
 namespace AdventOfCode24
-// https://www.reddit.com/r/adventofcode/comments/1hfhgl1/2024_day_16_part_1_alternate_test_case/
 
 open System
 
 type D16Pos = int*int
 type D16Dir = Up | Right | Down | Left
-
-type D16Node = {MinDist : int; Dir : D16Dir option; Paths : D16Pos list list}
+type D16Node = {MinDist : int; Dir : D16Dir option; Prev : D16Pos; Paths : D16Pos list list}
 
 type Day16() =
     static let mutable _sPos = (0,0)
@@ -20,56 +18,41 @@ type Day16() =
         with get() = _ePos
         and set(p) = _ePos <- p
     
-    static member private nextPos ((r,c) : D16Pos) : D16Dir -> D16Pos = function
-        | Up -> (r-1,c) | Right -> (r,c+1) | Down -> (r+1,c) | Left -> (r,c-1)
-    
-    static member private connections ((r,c) : D16Pos) (blocked : bool) (dir : D16Dir) : (D16Pos*D16Dir*int)[] =
-        let dst = if blocked then 1 else 1001
-        match dir with
-        | Up ->    [|((r,c-1), Left,  dst); ((r-1,c), Up,    1); ((r,c+1), Right, dst)|]
-        | Right -> [|((r-1,c), Up,    dst); ((r,c+1), Right, 1); ((r+1,c), Down,  dst)|]
-        | Down ->  [|((r,c+1), Right, dst); ((r+1,c), Down,  1); ((r,c-1), Left,  dst)|]
-        | Left ->  [|((r+1,c), Down,  dst); ((r,c-1), Left,  1); ((r-1,c), Up,    dst)|]
+    static member private connections ((r,c) : D16Pos) : D16Dir -> (D16Pos*D16Dir*int)[] = function
+        | Up ->    [|((r,c-1), Left,  1001); ((r-1,c), Up,    1); ((r,c+1), Right, 1001)|]
+        | Right -> [|((r-1,c), Up,    1001); ((r,c+1), Right, 1); ((r+1,c), Down,  1001)|]
+        | Down ->  [|((r,c+1), Right, 1001); ((r+1,c), Down,  1); ((r,c-1), Left,  1001)|]
+        | Left ->  [|((r+1,c), Down,  1001); ((r,c-1), Left,  1); ((r-1,c), Up,    1001)|]
 
-    // unvisited : unvisited nodes
-    // queue : nodes to be handled, indexed by distance
-    // graph : nodes
-    // map : input map
     static member private path (unvisited : D16Pos list) (map : char[,]) (queue : Map<int, D16Pos list>) (graph : Map<D16Pos, D16Node>) : Map<D16Pos, D16Node> =
-        if queue.IsEmpty then graph // no more to handle
+        if queue.IsEmpty then graph
         else
             let (dst, nodePos::rest) = queue |> Map.minKeyValue
-            let (x,y) = nodePos
             let queue = if rest.IsEmpty then queue |> Map.remove dst else queue.Add (dst, rest)
 
             if not (List.contains nodePos unvisited) then Day16.path unvisited map queue graph
-            else // position is unvisited
+            else
                 let node = graph.[nodePos]
                 let unvisited = unvisited |> List.filter (fun p -> p <> nodePos)
 
-                let blocked = let (i,j) = Day16.nextPos nodePos node.Dir.Value in map.[i,j] = '#'
+                let paths = graph.[node.Prev].Paths |> List.map (fun path -> nodePos::path)
 
                 let (queue,graph) =
-                    Day16.connections nodePos blocked node.Dir.Value
-                    |> Array.map (fun (nbPos, nbDir, nbD) ->
-                        let (r,c) = nbPos
-                        if map.[r,c] = '#' then None
-                        else
-                            let nbD = let (i,j) = Day16.nextPos nbPos nbDir in if (r,c) <> Day16.EPos && map.[i,j] = '#' then nbD+1000 else nbD
-                            (nbPos, nbDir, node.MinDist+nbD) |> Some)
-                    |> Array.fold (fun (queue : Map<int, D16Pos list>, graph : Map<D16Pos, D16Node>) (o : (D16Pos*D16Dir*int) option) ->
-                        if o.IsNone then (queue,graph)
-                        else
-                            let (nbPos,nbDir,nbND) = o.Value
+                    Day16.connections nodePos node.Dir.Value |> Array.filter (fun ((r,c), _, _) -> map.[r,c] <> '#')
+                    |> Array.fold (fun (queue : Map<int, D16Pos list>, graph : Map<D16Pos, D16Node>) (nbPos,nbDir,nbND) ->
+                            let nbND = node.MinDist+nbND
                             let queue = queue.Add (nbND, nbPos::(if (queue.ContainsKey nbND) then queue.[nbND] else []))
                             let graph =
                                 let nb = graph.[nbPos]
                                 if nbND <= nb.MinDist then
-                                    let updatedPaths = nb.Paths @ (node.Paths |> List.map (fun path -> nbPos::path))
-                                    graph.Add (nbPos, {MinDist = nbND; Dir = Some nbDir; Paths = updatedPaths})
+                                    let mergedPaths = nb.Paths @ (paths |> List.map (fun path -> nbPos::path))
+                                    graph.Add (nbPos, {MinDist = nbND; Dir = Some nbDir; Prev = nodePos; Paths = mergedPaths})
                                 else graph
-
                             (queue,graph)) (queue,graph)
+
+                let graph =
+                    if nodePos = Day16.EPos then graph
+                    else graph.Add (nodePos, {MinDist = node.MinDist+1000; Dir = node.Dir; Prev = node.Prev; Paths = node.Paths})
                 
                 Day16.path unvisited map queue graph
 
@@ -80,45 +63,43 @@ type Day16() =
         let mutable queue = Map.empty<int,D16Pos list>
 
         parsed |> Array2D.iteri (fun r c s ->
-            if s = 'E' then
+            if s = 'S' then
                 Day16.SPos <- (r,c)
-                let (i,j) = Day16.nextPos (r,c) Left
-                let dist = if parsed.[i,j] = '#' then 1000 else 0
-                graph <- graph.Add ((r,c), {MinDist = dist; Dir = Some Left; Paths = [[(r,c)]]})
-                queue <- queue.Add (dist, [(r,c)])
+                graph <- graph.Add ((r,c), {MinDist = 0; Dir = Some Right; Prev = (r,c); Paths = [[(r,c)]]})
+                queue <- queue.Add (0, [(r,c)])
                 unvisited <- (r,c)::unvisited
-            elif s = 'S' then
+            elif s = 'E' then
                 Day16.EPos <- (r,c)
-                graph <- graph.Add ((r,c), {MinDist = Int32.MaxValue; Dir = None; Paths = []})
+                graph <- graph.Add ((r,c), {MinDist = Int32.MaxValue; Dir = None; Prev = (r,c); Paths = []})
                 unvisited <- (r,c)::unvisited
             elif s = '.' then
-                graph <- graph.Add ((r,c), {MinDist = Int32.MaxValue; Dir = None; Paths = []})
+                graph <- graph.Add ((r,c), {MinDist = Int32.MaxValue; Dir = None; Prev = (r,c); Paths = []})
                 unvisited <- (r,c)::unvisited)
 
         let graph = Day16.path unvisited parsed queue graph
-
-        if false then
-            Day16.mapGraph graph parsed
-        else
-            for (i,j) in (graph.[Day16.EPos].Paths |> List.concat |> List.distinct) do
-                parsed.[i,j] <- 'X'
-            parsed |> Day16.printMap
         
-        graph.[Day16.EPos].Paths |> List.concat |> List.distinct |> List.length |> printfn "%A"
         graph.[Day16.EPos].MinDist |> string
 
-    static member Star2 (input : string[]) : string = // 442
-        ""
+    static member Star2 (input : string[]) : string =
+        let parsed = input |> array2D
+        let mutable graph = Map.empty<D16Pos,D16Node>
+        let mutable unvisited : D16Pos list = []
+        let mutable queue = Map.empty<int,D16Pos list>
 
-    static member private mapGraph (graph : Map<D16Pos,D16Node>) (map : char[,]) : unit =
-        let helper : D16Dir -> char = function
-            | Up -> '^' | Right -> '>' | Down -> 'v' | Left -> '<'
+        parsed |> Array2D.iteri (fun r c s ->
+            if s = 'S' then
+                Day16.SPos <- (r,c)
+                graph <- graph.Add ((r,c), {MinDist = 0; Dir = Some Right; Prev = (r,c); Paths = [[(r,c)]]})
+                queue <- queue.Add (0, [(r,c)])
+                unvisited <- (r,c)::unvisited
+            elif s = 'E' then
+                Day16.EPos <- (r,c)
+                graph <- graph.Add ((r,c), {MinDist = Int32.MaxValue; Dir = None; Prev = (r,c); Paths = []})
+                unvisited <- (r,c)::unvisited
+            elif s = '.' then
+                graph <- graph.Add ((r,c), {MinDist = Int32.MaxValue; Dir = None; Prev = (r,c); Paths = []})
+                unvisited <- (r,c)::unvisited)
 
-        graph |> Map.iter (fun (r,c) node -> if node.Dir.IsSome then map.[r,c] <- helper (node.Dir.Value))
-        map |> Day16.printMap
-
-    static member private printMap (map : char[,]) : unit =
-        for i in [0..(Array2D.length1 map)-1] do
-            for j in [0..(Array2D.length2 map)-1] do
-                printf "%c" map.[i,j]
-            printfn ""
+        let graph = Day16.path unvisited parsed queue graph
+        
+        graph.[Day16.EPos].Paths |> List.concat |> List.distinct |> List.length |> string
