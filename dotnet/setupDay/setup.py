e36aa935d13648a8b17396f3b4582279ad602060
Marius <business@mottosen.xyz>
2025-12-01 19:01:10 +0100

    separated stuff a bit for a new year

--- dotnet/setupDay/setup.py
+++ dotnet/setupDay/setup.py
@@ -1,19 +1,22 @@
+import shutil
 import sys
 from pathlib import Path
-import shutil
 
-curr_dir = Path('.')
+curr_dir = Path(".")
 advent_dir = curr_dir / "../AOC24"
+
 
 def main(args):
 
     if len(args) != 2 or not args[1].isdigit():
         print("Program must take exactly one argument, the day to setup.")
         return 1
-    
+
     day = str(int(args[1])).zfill(2)
-    
-    if (advent_dir / f"inputs/day{day}.txt").is_file() or (advent_dir / f"Day{day}.fs").is_file():
+
+    if (advent_dir / f"inputs/day{day}.txt").is_file() or (
+        advent_dir / f"Day{day}.fs"
+    ).is_file():
         print("some important files already exist.")
         return 1
 
@@ -22,13 +25,14 @@ def main(args):
 
     with open(advent_dir / f"inputs_test/Day{day}_1.txt", "w") as f:
         f.write("")
-    
+
     with open(advent_dir / f"inputs_test/Day{day}_2.txt", "w") as f:
         f.write("")
 
     shutil.copyfile(curr_dir / "NewDay.fs", advent_dir / f"Days/Day{day}.fs")
-    
+
     return 0
+
 
 if __name__ == "__main__":
     main(sys.argv)
