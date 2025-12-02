0fa2b50da8c972950a3791b911b7b47759f0909e
Marius <business@mottosen.xyz>
2025-12-02 13:32:17 +0100

    reorganized

--- python/AOC25/Day2/Parts/Util/handlers.py
+++ python/AOC25/Day2/Parts/Util/handlers.py
@@ -1,2 +1,15 @@
+import re
+
+
 def handle_range(handle_id, id_range):
-    return list(filter(lambda id: id is not None, map(handle_id, id_range)))
+    return list(map(handle_id, id_range))
+
+
+# regex: same number series exactly twice
+def handle_id_2(id):
+    return id if re.match(r"^(\d+)\1$", str(id)) else 0
+
+
+# regex: same number series at least twice
+def handle_id_n(id):
+    return id if re.match(r"^(\d+)\1+$", str(id)) else 0
--- python/AOC25/Day2/Parts/part2.py
+++ python/AOC25/Day2/Parts/part2.py
@@ -1,33 +1,27 @@
 import itertools
-import multiprocessing
+import multiprocessing as mp
 import re
 from functools import partial
 from pathlib import Path
 
-from Parts.Util.handlers import handle_range
-
-
-def handle_id(id):
-    # regex: same number series at least twice
-    match = re.match(r"^(\d+)\1+$", str(id))
-    if match:
-        return id
+import Parts.Util.handlers as handlers
 
 
 class Part2:
     def __init__(self, input_file: Path):
         with open(input_file, "r") as f:
-            matches: list(str) = re.findall(r"(\d+)-(\d+)", f.readline())
+            matches = re.findall(r"(\d+)-(\d+)", f.readline())
 
-        self.worker_pool = multiprocessing.Pool()
+        self.worker_pool = mp.Pool()
         self.id_ranges = [range(int(match[0]), int(match[1]) + 1) for match in matches]
 
     def get_day_solution(self):
         return str(
             sum(  # summing invalid ids
-                itertools.chain(  # chaining resulting lists
-                    *self.worker_pool.map(  # ranges are handled in parallel
-                        partial(handle_range, handle_id), self.id_ranges
+                itertools.chain(  # chaining results
+                    *self.worker_pool.map(  # handle ranges in parallel
+                        partial(handlers.handle_range, handlers.handle_id_n),
+                        self.id_ranges,
                     )
                 )
             )
